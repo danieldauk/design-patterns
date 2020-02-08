61a2ae8fe1165257089fd0c80aee06ce698ca66e
Daniel Dauksevic <danieldauk@gmail.com>
2020-02-08 14:21:40 +0200

    feat: add prototype pattern

--- creational-patterns/prototype.ts
+++ creational-patterns/prototype.ts
@@ -1,1 +1,39 @@
+interface BikePrototype {
+  clone(): BikePrototype;
+}
+
+
+class Bike implements BikePrototype {
+  numberOfWheels = 2;
+  createdAt = new Date();
+
+  clone() {
+    const clone = Object.create(this);
+    clone.createdAt = new Date(this.createdAt.getTime());
+
+    return clone;
+  }
+}
+
+const clientCode = () => {
+  const bike = new Bike();
+  const clone = bike.clone();
+
+  const arePrimitiveValuesEqual = bike.numberOfWheels === clone.numberOfWheels;
+  if (arePrimitiveValuesEqual) {
+    console.log('Primitive field value has been cloned');
+  }
+
+  const areDifferentReferences = bike.createdAt !== clone.createdAt;
+  const areReferenceValuesEqual = bike.createdAt.getTime() === clone.createdAt.getTime();
+
+  if (areDifferentReferences && areReferenceValuesEqual) {
+    console.log('Reference value has been cloned')
+  }
+}
+
+clientCode();
+// Primitive field value has been cloned
+// Reference value has been cloned
+
 export {}
