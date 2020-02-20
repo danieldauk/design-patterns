f2647fe22a0bc0fe477601ffcc37ccf9c1e24775
Daniel Dauksevic <danieldauk@gmail.com>
2020-02-20 19:04:49 +0200

    fix: typo

--- behavioural-patterns/observer.ts
+++ behavioural-patterns/observer.ts
@@ -33,7 +33,7 @@ class Subscriber implements SubscriberInterface {
     this.name = name;
   }
   update(issue: string) {
-    console.log(`${this.name} latest magazine issue: ${issue}`);
+    console.log(`${this.name} received latest magazine issue: ${issue}`);
   }
 }
 
@@ -46,12 +46,12 @@ const clientCode = () => {
   magazine.subscribe(joseph);
 
   magazine.setCurrentIssue('Issue 1'); 
-  // Alan latest magazine issue: Issue 1
-  // Joseph latest magazine issue: Issue 1
+  // Alan received latest magazine issue: Issue 1
+  // Joseph received latest magazine issue: Issue 1
   magazine.unsubscribe(alan);
 
   magazine.setCurrentIssue('Issue 2');
-  // Joseph latest magazine issue: Issue 2
+  // Joseph received latest magazine issue: Issue 2
 };
 
 clientCode();
